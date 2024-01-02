de60a62b38dd8557d02252577c610419525cbb42
Precious <nafiseh.maraghi@gmail.com>
2024-01-02 15:55:47 +0330

    SimpleJWT and login APIs added

--- user/api/urls.py
+++ user/api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from . import views as custom_views
 
 
 urlpatterns = [
     path('signup/', custom_views.SignupAPIView.as_view(), name='signup'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
